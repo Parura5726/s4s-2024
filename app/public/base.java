5d4af942a922f8ed3e6510ee5d094d22fec5ec9b
SekoiaTree <sekoia@sekoia.dev>
2025-09-02 13:56:19 +0200

    fix: adjust some wording

--- app/public/base.java
+++ app/public/base.java
@@ -17,13 +17,14 @@ public class Base {
     // Fonction pour trouver les coups à jouer
     public static List<Move> findMove(Piece[][] board, char playerColor, List<ArrayList<Move>> possibleMoves) {
 
-        // TODO: Implémentez ici la logique pour trouver les coups à jouer et les retourner
+        // TODO: Implémentez ici la logique pour choisir les coups à jouer et les retourner
         // Les coups doivent être retournés sous forme d'une liste d'objets Move,
         // Chaque objet Move représente un coup, avec une cellule de départ et une cellule d'arrivée
         // Les classes Position(row, column) et Move(from, to) sont fournies pour vous
 
         //System.out.println("You can print your debug values here!");
 
+        // possibleMoves contient toutes les séquences de mouvements possibles.
         return possibleMoves.get(0);
     }
 }
